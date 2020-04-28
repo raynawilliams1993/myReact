ab0493f2e1d4e74cfb95a03df31961d9697a42fb
rayna williams <Raynawilliams123@gmail.com>
2020-04-28 15:47:11 -0600

    added my hero,footer, col, and row components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import navigate from "./components/navigate/navigate";
-// import Wrapper from "./client/components/wrapper/wrapper";
+import Wrapper from "./client/components/wrapper/wrapper";
 import About from "./components/pages/about";
 import Contact from "./components/pages/contact";
 import Portfolio from "./components/pages/portfolio";
 // import LandingPage from "./client/components/landingPage/landingPage";
-// import Footer from "./components/footer/footer";
+import Footer from "./components/footer/footer";
 
 
 function App() {
@@ -15,15 +15,15 @@ function App() {
     <Router>
       <div className="App">
         <navigate />
-{/* 
-        <Wrapper> */}
+
+        <Wrapper>
           {/* <Route path="/" render={() => <LandingPage />} exact /> */}
-          <Route path="/about" render={() => <About />} exact />
+          <Route path="/" render={() => <About />} exact />
           <Route path="/contact" render={() => <Contact />} exact />
           <Route path="/portfolio" render={() => <Portfolio />} exact />
-{/*           
-        </Wrapper> */}
-      {/* <Footer/> */}
+
+        </Wrapper>
+        <Footer />
       </div>
     </Router>
   );
