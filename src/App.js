8a6aac8904bd7f45cff880a446157f5300519b45
rayna williams <Raynawilliams123@gmail.com>
2020-04-30 16:06:36 -0600

    got my landingPage working, need to add a cool navbar to all pagesss still

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ import Wrapper from "./components/wrapper/wrapper";
 import About from "./components/pages/about";
 import Contact from "./components/pages/contact";
 import Portfolio from "./components/pages/portfolio";
-// import LandingPage from "./client/components/landingPage/landingPage";
+import LandingPage from "./components/landingPage/landingPage";
 import Footer from "./components/footer/footer";
-import breadNav from "./components/breadCrumb/breadCrumb";
+
 
 function App() {
   return (
 
     <Router>
       <div className="App">
-        <breadNav/>
+  
 
         <Wrapper>
-          {/* <Route path="/" render={() => <LandingPage />} exact /> */}
-          <Route path="/" render={() => <About />} exact />
+          <Route path="/" render={() => <LandingPage />} exact />
+          <Route path="/about" render={() => <About />} exact />
           <Route path="/contact" render={() => <Contact />} exact />
           <Route path="/portfolio" render={() => <Portfolio />} exact />
 
---
+++ src/components/landingPage/landingPage.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import {
+  Hero
+} from 'react-landing-page'
+
+const LandingPage = props => (
+ 
+    <Hero
+      
+      color="white"
+        // bg="white"
+
+      backgroundImage="https://images.wallpaperscraft.com/image/earth_moon_space_galaxy_118437_1280x1280.jpg"
+    >
+      <h1>Rayna K. Williams</h1>
+      <h2>FULL STACK WEB DEVELOPER</h2>
+
+
+
+
+
+    </Hero>
+
+)
+export default LandingPage;
